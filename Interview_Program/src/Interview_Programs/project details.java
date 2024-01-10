b20d2201397891f7d3295c71ec4cf09a328d301f
SomeshGhuge <113898033+SomeshGhuge@users.noreply.github.com>
2024-01-10 15:22:37 +0530

    Add files via upload

---
+++ Interview_Program/src/Interview_Programs/project details.java
@@ -0,0 +1,25 @@
+
+PROJECT :
+
+1) Poleepo :
+
+   @) get data from Server through httprequest 
+        for that we use JWT token , HTTp response , Configure that data in to DTO  
+   @) Now we can pass thos values with proper integrating and mapping 
+   @) That values are passed throug predefined dependenciey and code is pushed into perticular barnch
+   
+   
+    update skills in this project  Spring Security, Integerrating concepts ,Core java Concepts , Database concepts , reading documnet and develop the code according to that documnet as per      client required , spring boot related skills , HTTP Request/Client, MySql Database , git related concepts , Different Queries, mapping  .
+   
+   
+2)RTSG (Rakuten Trade Singapur Exchange) :
+     
+   @) Receive the data through the xml by using Kafka services 
+   @) Bind this xml data with models by using JAXBCONTEXT 
+   @) modify bussiness logic with model  
+   @) Entities used to store data in database 
+   @) Kafka services used for produce the data and consume the data  after sucessfully complition of code 
+   @) Receive the data from SGX Server using Kafka 
+   
+   update skills related to Advance java , core java ,Docker, reactive programming, Kafka service(Produce/Consume), Database PostgresSQL, How ro deal with diff modules and working with modules ,
+   Stream api ( map, flatmap, forEach, lambda functions ...etc) , mapping with reactive programming...etc
